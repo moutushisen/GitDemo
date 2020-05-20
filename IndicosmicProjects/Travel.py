f3aac19b90e6af4f6ea2de43b91cb88a2ccc7f96
Moutushi <moutushisen@gmail.com>
2020-05-20 14:30:09 +0530

    first commit

---
+++ IndicosmicProjects/Travel.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+from selenium.webdriver.support.select import Select
+
+#driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+
+driver.get("http://uat.mypolicynow.com/")
+
+driver.find_element_by_id("open-login-pop-up").click()
+driver.find_element_by_name("username").send_keys("9987793988")
+# driver.find_element_by_css_selector("input[name='name']").send_keys("9987793988")
+driver.find_element_by_name("password").send_keys("9987793988")
+driver.find_element_by_id("password").click()
+driver.find_element_by_xpath("//button[@id='login_submit']").click()
+
+driver.back()
+driver.get("http://uat.mypolicynow.com/quotation/travel")
+
+driver.refresh()
+driver.close()
